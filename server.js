7649f1f0e38f62326a202fb2053a84c8ad108960
Ken Ruf <ruf.kenichi@gmail.com>
2020-03-03 11:01:28 -0600

    add userRouter to server.js

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ const server = express();
 const helmet = require("helmet");
 const cors = require("cors");
 
+const userRouter = require("./user/userRouter")
+
 server.use(express.json());
 server.use(helmet());
 server.use(cors());
+
+server.use("/api/user/register", userRouter);
 
 server.get("/", (req, res) => {
     res.send("Backend Server for GrowthLoop")
