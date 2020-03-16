2b0702e37aa4ce7b1d6d5fe02069fbbeaef453d1
Ken Ruf <ruf.kenichi@gmail.com>
2020-03-16 10:31:50 -0500

    add eventRouter to server

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cors = require("cors");
 const userRouter = require("./user/userRouter")
 const orgRouter = require("./organization/organizationRouter")
 const funnelRouter = require("./funnel/funnelRouter")
+const eventRouter = require("./event/eventRouter")
 
 server.use(express.json());
 server.use(helmet());
@@ -14,6 +15,7 @@ server.use(cors());
 server.use("/api/users", userRouter);
 server.use("/api/orgs", orgRouter);
 server.use("/api/funnels", funnelRouter);
+server.use("/api/events", eventRouter);
 
 server.get("/", (req, res) => {
     res.send("Backend Server for GrowthLoop")
